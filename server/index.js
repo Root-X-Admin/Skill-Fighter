c41185a814a289678d1500a5c3fc4ceb4bac9972
Root-X-Admin <ayushhande052@gmail.com>
2025-07-11 14:42:14 +0530

    refactor(package.json, index.js): simplify start script and server listen configuration

--- server/index.js
+++ server/index.js
@@ -41,4 +41,4 @@ app.get('/', (req, res) => {
 
 // 🚀 Start server
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
